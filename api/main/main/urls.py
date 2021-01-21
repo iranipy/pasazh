84316424a90c8ecc95697d2725854ec1b6ad6df5
AlirezaYeganeh <AlirezaYeganeh41@yahoo.com>
2021-01-21 13:36:33 +0330

    fix urls

--- api/main/main/urls.py
+++ api/main/main/urls.py
@@ -4,9 +4,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/auth', include('authentication.urls')),
-    path('/user', include('user.urls')),
-    path('/salesman', include('salesman.urls')),
-    path('/business', include('business.urls')),
+    path('auth/', include('authentication.urls')),
+    path('user/', include('user.urls')),
+    path('salesman/', include('salesman.urls')),
+    path('business/', include('business.urls')),
     # path('', include('root.urls')),
 ]
