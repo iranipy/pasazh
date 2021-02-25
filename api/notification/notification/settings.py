a727b9a18ce81c5ad3a45c31b2818f23d24b761b
RezaRjbi <reza.rajabi61925@gmail.com>
2021-02-25 12:01:02 +0330

    install rest_framwork

--- api/notification/notification/settings.py
+++ api/notification/notification/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
     'sms.apps.SmsConfig',
     'mail.apps.MailConfig',
 ]
